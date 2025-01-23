5b95a957772d82fe4fa0725547fbcb9f8c46211d
freedform <62791746+freedform@users.noreply.github.com>
2025-01-23 17:28:40 +0500

    some commit

--- netbox-docker/elk_alerts/main.py
+++ netbox-docker/elk_alerts/main.py
@@ -12,7 +12,7 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         device_filter = query_components.get("filter", [])
         result = {
             "core_sw_1": "ok",
-            "core_sw_2": "error",
+            "core_sw_2": "ok",
             "dist_sw_1": "ok",
             "dist_sw_2": "warning",
             "edge_ro_1": "ok",
